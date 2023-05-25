b8be427344a918b3fc4ceb9c36a8b6aa4a6d466f
Vinny Surya <vinnysuryatrinita@gmail.com>
2023-05-25 13:51:19 +0700

    update perubahan ke2

--- View/html/Login Register/login.php
+++ View/html/Login Register/login.php
@@ -2,39 +2,42 @@
 <html>
   <head>
     <title>Login Form</title>
-	<link rel="stylesheet" type="text/css" href="regis-login.css">
+    <link rel="stylesheet" type="text/css" href="regis-login.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-	<script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
+    <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
   </head>
   <body>
-  <div class="container-fluid">
-		<div class="row">
-			<div class="col-sm-6" style="background-color: #DA7F8F;">
-				<i class="iconify" data-icon="icon-park-solid:baby-feet"></i>
-				<div class="left-text">
-					<p>WELCOME TO</p>
-					<h1>BabyXpress</h1>
-					<h6>Baby problems? We gotchu</h6>
-				</div>
-				<i class="iconify" data-icon="mdi:baby-carriage"></i>
+    <div class="container-fluid">
+		  <div class="row">
+			  <div class="col-sm-6" style="background-color: #DA7F8F;">
+				  <div class="icon2">
+					  <img src="babyfeet.png" class="img-fluid">
+				  </div>				
+				  
+          <div class="left-text">
+					  <p>WELCOME TO</p>
+					  <h1>BabyXpress</h1>
+					  <h6>Baby problems? We gotchu</h6>
+				  </div>
+				  <i class="iconify" data-icon="mdi:baby-carriage"></i>
+			  </div>
 
-			</div>
-			<div class="col-sm-6" style="background-color: #fff;">
-				<h2>Login</h2>
-				
-				<form action="loginController.php" method="POST">
+			  <div class="col-sm-6" style="background-color: #fff;">
+				  <h2>Login</h2>	
+				  <form action="loginController.php" method="POST">
 					<p>Login As<span class="red-text">*</span></p>
-					<select class="form-select" aria-label="Login As">
+					
+          <select class="form-select" aria-label="Login As">
 						<option value="1">Customer</option>
 						<option value="2">Babysitter</option>
 					</select>
 
 					<div class="form-group">
-						<label for="username">Username<span class="red-text">*</span></label>
-						<input type="text" class="form-control" name="username" id="username" placeholder="Enter your username">							
+						<label for="email">Email<span class="red-text">*</span></label>
+						<input type="text" class="form-control" name="email" id="email" placeholder="Enter your email">							
 					</div>
 					<div class="form-group">
 						<label for="password">Password<span class="red-text">*</span></label>
