4b5fa7d33a992463dea8d277bf95583229678b77
mandaania <amandanathania189@gmail.com>
2023-05-25 13:55:18 +0700

    Edit Login Register

--- Model/Login Register/User.php
+++ Model/Login Register/User.php
@@ -1,21 +1,30 @@
 <?php
 include_once("Email.php");
 class User{
+  private $conn;
+  private $table = 'user'; 
+  private $id;
+  private $db;
 
     private $username;
     private $password;
     private $emailUser;
+    public function __construct($conn){
+      $this->conn = '$db';
+    }
 
     public function doLogin($username,$password){
-        $this->username = $username;
-        $this->password = $password;
-       if($this->username == '' || $this->password == ''){
-         return false;
-       }
-       else{
-         return true;
-       }
+        $query = 'SELECT * FROM ' . $this->table . 'WHERE username = :username';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':username', $this->username);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO:FETCH_ASSOC);
 
+        if($row && password_verify($this->password, $row['password'])){
+          $this->id = $row['id'];
+          return true;
+        }
+        return false;
     }
 
     public function doRegister($username,$password,$emailUser){
