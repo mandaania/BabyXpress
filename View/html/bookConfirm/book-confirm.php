e14d0281873c5e03666cad1e102673ecfa8fe394
DeSul <126030381+DenimoSullivan@users.noreply.github.com>
2023-06-19 10:43:40 +0700

    add file untuk page Booking Confirm
    List customer yang melakukan booking ke Babysitter itu.

---
+++ View/html/bookConfirm/book-confirm.php
@@ -0,0 +1,55 @@
+<?php
+$localhost = 'DESKTOP-FGQRAI2'
+$user = 'root'
+$pass = 'root'
+$db = 'dbbabyxpress'
+
+// mysqli instance
+$mysqli = new mysqli($localhost, $user, $pass, $db);
+
+// check mysqli connection
+if ($mysqli->connect_error) {
+    die('Connection failed: ' . $mysqli->connect_error);
+}
+
+$query = "SELECT name, profile_pic FROM user";
+
+$result = $pdo->query($query);
+$cust = $result->fetchAll(PDO::FETCH_ASSOC);
+
+// ambil data profile pic cust n namanya
+foreach ($cust as $cust) {
+    echo '<div class="customer">';
+    echo '<img src="' . $customer['profile_pic'] . '" alt="Profile Picture">';
+    echo '<p>' . $customer['name'] . '</p>';
+    echo '</div>';
+}
+
+$mysqli->close();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="book-confirm.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css">
+        <title>Booking Confirmation</title>
+    </head>
+    <body>
+        <div class="navbar">
+            <!-- hamburger menu -->
+            <h1>Booking Confirmation</h1>
+            <button class="back-btn" >Back</button>
+            <button class="logout-btn">Logout</button>
+        </div>
+        <div class="customer-list">
+            <div class="container">
+                <!-- the background with color A7BBC7 -->
+                <!-- rectangle buat background cust profile pic n name -->
+                <button type="button" class="btn btn-light">Confirm</button>
+                <span class="text-wrap text-capitalize fs-6 fw-light">John Doe</span>
+            </div>
+        </div>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.min.js"></script>
+    </body>
+</html>
