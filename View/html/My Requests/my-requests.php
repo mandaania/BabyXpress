8051227183e7e8b6164ce6b0c191b99ff298c760
DenimoSullivan <taela022@gmail.com>
2023-06-19 21:11:50 +0700

    update my-requests feature

--- View/html/My Requests/my-requests.php
+++ View/html/My Requests/my-requests.php
@@ -31,27 +31,22 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" href="../../../../css/Booking Confirm/my-requests.css">
-        <link rel="stylesheet" href="../../../../css/NavBar/navbar-manda.css">
+        <link rel="stylesheet" href="../../../../css/My Requests/my-requests.css">
+        <link rel="stylesheet" href="../../../../css/NavBar/navbar-my-requests.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css">
-        <title>Booking Confirmation</title>
+        <title>My Requests</title>
     </head>
     <body>
-        <?php include_once("../../css/NavBar/navbar-manda.css")?>
-        <div class="navbar">
-            <h1>Booking Confirmation</h1>
-            <button class="back-btn" >Back</button>
-            <button class="logout-btn">Logout</button>
-        </div>
-        <div class="customer-list">
-            <div class="container">
-                <!-- the background with color A7BBC7 -->
-                <!-- rectangle buat background cust profile pic n name -->
-                <button type="button" class="btn btn-light">Confirm</button>
-                <span class="text-wrap text-capitalize fs-6 fw-light">John Doe</span>
+        <div class="container-xl">
+            <div class="customer-list">
+                <div class="container">
+                    <!-- the background with color A7BBC7 -->
+                    <!-- rectangle buat background cust profile pic n name -->
+                    <button type="button" class="btn btn-light">Confirm</button>
+                    <span class="text-wrap text-capitalize fs-6 fw-light">John Doe</span>
+                </div>
             </div>
         </div>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.min.js"></script>
-
     </body>
 </html>
